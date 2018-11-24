b7ae515513b9efd71d982974687e23138dbcbc3e
Paul Botros <pbotros1234@gmail.com>
2018-11-23 21:21:57 -0800

    dont think this is it

--- src/learn_decoder.m
+++ src/learn_decoder.m
@@ -3,8 +3,7 @@ function [model] = learn_decoder(V, X)
     % V is the "real". Should be numTrials x numOutputs
     % X is the input. Should be numTrials x numInputs
 
-    eta = 0.0001;
-    num_iterations = 50;
+    num_iterations = 1000;
     l_s = 1;
     l_e = 1;
 
@@ -16,9 +15,8 @@ function [model] = learn_decoder(V, X)
     A = randn(num_inputs, 4);
     b = randn(1, 4);
     errs = zeros(num_iterations, num_outputs);
-    figure;
-    for iterationIdx = 1:num_iterations
-        disp(iterationIdx);
+
+    function [theta_s, theta_e, omega_s, omega_e, output] = go_forward(X, A, b)
         intermediate = X * A + b;
         theta_s = intermediate(:, 1);
         theta_e = intermediate(:, 2);
@@ -26,8 +24,17 @@ function [model] = learn_decoder(V, X)
         omega_e = intermediate(:, 4);
         output = [ ...
             -l_s * sin(theta_s) .* omega_s - l_e * sin(theta_e) .* omega_e, ...
-            -l_s * cos(theta_s) .* omega_s + l_s * cos(theta_e) .* omega_e ...
+             l_s * cos(theta_s) .* omega_s + l_s * cos(theta_e) .* omega_e ...
         ];
+    end
+
+    for iterationIdx = 1:num_iterations
+        [theta_s, theta_e, omega_s, omega_e, output] = go_forward(X, A, b);
+
+        cost = sum(0.5 * (V - output).^2);
+        disp(sprintf('Iteration %d: Cost %.8f \n', iterationIdx, cost(1) + cost(2)));
+        drawnow('update');
+
         err = abs(V - output);
         errs(iterationIdx, :) = sum(abs(err), 1);
         de_daij = zeros(num_trials, size(A, 1), size(A, 2));
@@ -47,18 +54,41 @@ function [model] = learn_decoder(V, X)
 
             for i = 1:size(A, 1)
                 for j = 1:size(A, 2)
-                    du_daij = zeros(4, 1);
+                    du_daij = zeros(1, 4);
                     du_daij(j) = X(trialIdx, i);
 
-                    dt_daij = (gradt * du_daij);
+                    dt_daij = gradt * du_daij';
                     de_daij(trialIdx, i, j) = err(trialIdx, :) * dt_daij;
                 end
             end
             de_db(trialIdx, :) = err(trialIdx, :) * gradt;
         end
-        summed_grad = squeeze(sum(de_daij, 1));
-        A = A - eta * summed_grad;
-        b = b - eta * sum(de_db, 1);
+        summed_grad = zeros(size(de_daij, 2), size(de_daij, 3));
+        for trialIdx = 1:num_trials
+            summed_grad = summed_grad + squeeze(de_daij(trialIdx, :, :));
+        end
+        summed_grad_b = sum(de_db, 1);
+
+        x = [];
+        eta = 1;
+        while 1
+            new_A = A + eta * summed_grad;
+            new_b = b + eta * summed_grad_b;
+            [~, ~, ~, ~, output] = go_forward(X, new_A, new_b);
+            new_cost = sum(0.5 * (V - output).^2);
+            if new_cost(1) >= cost(1) || new_cost(2) >= cost(2)
+                if eta < 1e-10
+                    A = new_A;
+                    b = new_b;
+                    break
+                end
+                eta = eta/(1 + 1e-3);
+            else
+                A = new_A;
+                b = new_b;
+                break
+            end
+        end
     end
 
     model = struct;
